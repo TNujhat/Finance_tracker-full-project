56ae8b32f3c726af9b31d713645674d11d6166b8
TNujhat <tnujhat10@gmail.com>
2025-10-17 03:38:10 +0600

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,21 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+   baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+   ...compat.extends("next/core-web-vitals", "next/typescript"),
+   {
+      rules: {
+         "@typescript-eslint/no-explicit-any": "off",
       },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+   },
+];
+
+export default eslintConfig;
